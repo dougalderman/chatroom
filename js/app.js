d29f7781d8200481f3ec31dc5f9029fee3dc5b2a
Doug Alderman <agnadcp-d1@yahoo.com>
2015-12-20 11:09:36 -0700

    12/20/15 changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('chatroom', []);
+var app = angular.module('chatroom', ['angularMoment']);
 
 app.config(function($httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
